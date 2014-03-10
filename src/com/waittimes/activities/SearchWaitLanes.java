ae610f86b8f40d992b1cf0dcd442ee08844b98ee
victor-fdez <victor.j.fdez@gmail.com>
2014-03-10 12:08:10 -0600

    added some pictures for icons used throughout the program

--- src/com/waittimes/activities/SearchWaitLanes.java
+++ src/com/waittimes/activities/SearchWaitLanes.java
@@ -9,24 +9,28 @@ import org.json.JSONObject;
 
 import com.waittimes.R;
 import com.waittimes.ui.JSONWaitLanesSearchAdapter;
-import com.waittimes.utilities.json.JSONGetTask;
+import com.waittimes.utilities.json.*;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
 
 
 public class SearchWaitLanes extends Activity {
-	public static String tag = SearchWaitLanes.class.getName();
+	public final static String tag = SearchWaitLanes.class.getName();
+	public final static String ACTIVITY_TITLE = "Add Wait Lanes";
 	private ListView list;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		this.setContentView(R.layout.search_wait_times);
 		this.list = (ListView)this.findViewById(R.id.searchListView);
-		
+		String domain = this.getString(R.string.domain);
+		ActionBar actionBar = this.getActionBar();
+		actionBar.setTitle(SearchWaitLanes.ACTIVITY_TITLE);
 		JSONGetTask task = new JSONGetTask();
 		try {
-			task.execute(new URI("http://www.cerberu.com:8001/WaitLanes/file/all/list.json"));
+			task.execute(new URI("http://"+domain+":8001/WaitLanes/file/all/list.json"));
 			JSONObject jsonObj = task.get();
 			JSONWaitLanesSearchAdapter adapter = new JSONWaitLanesSearchAdapter(this, jsonObj);
 			this.list.setAdapter(adapter);
